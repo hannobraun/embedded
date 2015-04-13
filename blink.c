85219ae2b4e231b57ab568f521f85c75544130da
Hanno Braun <mail@hannobraun.de>
2015-04-13 14:15:24 +0200

    Use native timer instead of delay function
    Again, this will look like black magic, if you don't already know what
    this is, but I'll go into it in a later blog article.
    
    If you want to study up before then, you can read about the Real-time
    timer in the Atmel SAM3X8E reference, chapter 13.
    
    The reference is available at:
    http://www.atmel.com/devices/SAM3X8E.aspx

--- blink.c
+++ blink.c
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+
+// Parallel I/O
 volatile int *pb_pio_enable          = (int *)0x400E1000;
 volatile int *pb_output_enable       = (int *)0x400E1010;
 volatile int *pb_set_output_data     = (int *)0x400E1030;
@@ -7,14 +9,30 @@ volatile int *pb_clear_output_data   = (int *)0x400E1034;
 
 int pb27_mask = 0x08000000;
 
+
+// Real-time Timer
+volatile int *timer_mode_register  = (int *)0x400E1A30;
+volatile int *timer_value_register = (int *)0x400E1A38;
+
+
+void sleep_ms(int milliseconds) {
+	int sleep_until = *timer_value_register + milliseconds;
+	while (*timer_value_register < sleep_until) {}
+}
+
+
 void setup() {
+	// Parallel I/O
 	*pb_pio_enable    = pb27_mask;
 	*pb_output_enable = pb27_mask;
+
+	// Real-time Timer
+	*timer_mode_register = 0x00000020;
 }
 
 void loop() {
 	*pb_set_output_data = pb27_mask;
-	delay(200);
+	sleep_ms(200);
 	*pb_clear_output_data = pb27_mask;
-	delay(800);
+	sleep_ms(800);
 }

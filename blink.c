f74e8347784d76880be6dc14006011d07192cd07
Hanno Braun <mail@hannobraun.de>
2015-04-13 13:29:44 +0200

    Use native PIO interface instead Arduino functions
    I realize this looks like black magic to anyone who isn't familiar with
    this stuff. I promise I'm going to explain all of it in a future blog
    post. Until then, you read up on it in the reference documentation for
    the Atmel SAM3X8E, chapter 31 and more specifically, chapter 31.7.
    
    The reference documentation is available at:
    http://www.atmel.com/devices/SAM3X8E.aspx

--- blink.c
+++ blink.c
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 
-int led = 13;
+volatile int *pb_pio_enable          = (int *)0x400E1000;
+volatile int *pb_output_enable       = (int *)0x400E1010;
+volatile int *pb_set_output_data     = (int *)0x400E1030;
+volatile int *pb_clear_output_data   = (int *)0x400E1034;
+
+int pb27_mask = 0x08000000;
 
 void setup() {
-	pinMode(led, OUTPUT);
+	*pb_pio_enable    = pb27_mask;
+	*pb_output_enable = pb27_mask;
 }
 
 void loop() {
-	digitalWrite(led, HIGH);
+	*pb_set_output_data = pb27_mask;
 	delay(200);
-	digitalWrite(led, LOW);
+	*pb_clear_output_data = pb27_mask;
 	delay(800);
 }

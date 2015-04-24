ae7b9abf9b4a9834e27a86277c7016e67897d5bb
Hanno Braun <mail@hannobraun.de>
2015-04-24 21:16:15 +0200

    Remove unused types

--- atmel/sam3x8e.h
+++ atmel/sam3x8e.h
@@ -41,13 +41,6 @@
 
 #if !(defined(__ASSEMBLY__) || defined(__IAR_SYSTEMS_ASM__))
 #include <stdint.h>
-#ifndef __cplusplus
-typedef volatile const uint32_t RoReg; /**< Read only 32-bit register (volatile const unsigned int) */
-#else
-typedef volatile       uint32_t RoReg; /**< Read only 32-bit register (volatile const unsigned int) */
-#endif
-typedef volatile       uint32_t WoReg; /**< Write only 32-bit register (volatile unsigned int) */
-typedef volatile       uint32_t RwReg; /**< Read-Write 32-bit register (volatile unsigned int) */
 #endif
 
 /* ************************************************************************** */
@@ -55,65 +48,6 @@ typedef volatile       uint32_t RwReg; /**< Read-Write 32-bit register (volatile
 /* ************************************************************************** */
 /** \addtogroup SAM3X8E_cmsis CMSIS Definitions */
 /*@{*/
-
-/**< Interrupt Number Definition */
-typedef enum IRQn
-{
-/******  Cortex-M3 Processor Exceptions Numbers ******************************/
-  NonMaskableInt_IRQn   = -14, /**<  2 Non Maskable Interrupt                */
-  MemoryManagement_IRQn = -12, /**<  4 Cortex-M3 Memory Management Interrupt */
-  BusFault_IRQn         = -11, /**<  5 Cortex-M3 Bus Fault Interrupt         */
-  UsageFault_IRQn       = -10, /**<  6 Cortex-M3 Usage Fault Interrupt       */
-  SVCall_IRQn           = -5,  /**< 11 Cortex-M3 SV Call Interrupt           */
-  DebugMonitor_IRQn     = -4,  /**< 12 Cortex-M3 Debug Monitor Interrupt     */
-  PendSV_IRQn           = -2,  /**< 14 Cortex-M3 Pend SV Interrupt           */
-  SysTick_IRQn          = -1,  /**< 15 Cortex-M3 System Tick Interrupt       */
-/******  SAM3X8E specific Interrupt Numbers *********************************/
-
-  SUPC_IRQn            =  0, /**<  0 SAM3X8E Supply Controller (SUPC) */
-  RSTC_IRQn            =  1, /**<  1 SAM3X8E Reset Controller (RSTC) */
-  RTC_IRQn             =  2, /**<  2 SAM3X8E Real Time Clock (RTC) */
-  RTT_IRQn             =  3, /**<  3 SAM3X8E Real Time Timer (RTT) */
-  WDT_IRQn             =  4, /**<  4 SAM3X8E Watchdog Timer (WDT) */
-  PMC_IRQn             =  5, /**<  5 SAM3X8E Power Management Controller (PMC) */
-  EFC0_IRQn            =  6, /**<  6 SAM3X8E Enhanced Flash Controller 0 (EFC0) */
-  EFC1_IRQn            =  7, /**<  7 SAM3X8E Enhanced Flash Controller 1 (EFC1) */
-  UART_IRQn            =  8, /**<  8 SAM3X8E Universal Asynchronous Receiver Transceiver (UART) */
-  SMC_IRQn             =  9, /**<  9 SAM3X8E Static Memory Controller (SMC) */
-  PIOA_IRQn            = 11, /**< 11 SAM3X8E Parallel I/O Controller A, (PIOA) */
-  PIOB_IRQn            = 12, /**< 12 SAM3X8E Parallel I/O Controller B (PIOB) */
-  PIOC_IRQn            = 13, /**< 13 SAM3X8E Parallel I/O Controller C (PIOC) */
-  PIOD_IRQn            = 14, /**< 14 SAM3X8E Parallel I/O Controller D (PIOD) */
-  USART0_IRQn          = 17, /**< 17 SAM3X8E USART 0 (USART0) */
-  USART1_IRQn          = 18, /**< 18 SAM3X8E USART 1 (USART1) */
-  USART2_IRQn          = 19, /**< 19 SAM3X8E USART 2 (USART2) */
-  USART3_IRQn          = 20, /**< 20 SAM3X8E USART 3 (USART3) */
-  HSMCI_IRQn           = 21, /**< 21 SAM3X8E Multimedia Card Interface (HSMCI) */
-  TWI0_IRQn            = 22, /**< 22 SAM3X8E Two-Wire Interface 0 (TWI0) */
-  TWI1_IRQn            = 23, /**< 23 SAM3X8E Two-Wire Interface 1 (TWI1) */
-  SPI0_IRQn            = 24, /**< 24 SAM3X8E Serial Peripheral Interface (SPI0) */
-  SSC_IRQn             = 26, /**< 26 SAM3X8E Synchronous Serial Controller (SSC) */
-  TC0_IRQn             = 27, /**< 27 SAM3X8E Timer Counter 0 (TC0) */
-  TC1_IRQn             = 28, /**< 28 SAM3X8E Timer Counter 1 (TC1) */
-  TC2_IRQn             = 29, /**< 29 SAM3X8E Timer Counter 2 (TC2) */
-  TC3_IRQn             = 30, /**< 30 SAM3X8E Timer Counter 3 (TC3) */
-  TC4_IRQn             = 31, /**< 31 SAM3X8E Timer Counter 4 (TC4) */
-  TC5_IRQn             = 32, /**< 32 SAM3X8E Timer Counter 5 (TC5) */
-  TC6_IRQn             = 33, /**< 33 SAM3X8E Timer Counter 6 (TC6) */
-  TC7_IRQn             = 34, /**< 34 SAM3X8E Timer Counter 7 (TC7) */
-  TC8_IRQn             = 35, /**< 35 SAM3X8E Timer Counter 8 (TC8) */
-  PWM_IRQn             = 36, /**< 36 SAM3X8E Pulse Width Modulation Controller (PWM) */
-  ADC_IRQn             = 37, /**< 37 SAM3X8E ADC Controller (ADC) */
-  DACC_IRQn            = 38, /**< 38 SAM3X8E DAC Controller (DACC) */
-  DMAC_IRQn            = 39, /**< 39 SAM3X8E DMA Controller (DMAC) */
-  UOTGHS_IRQn          = 40, /**< 40 SAM3X8E USB OTG High Speed (UOTGHS) */
-  TRNG_IRQn            = 41, /**< 41 SAM3X8E True Random Number Generator (TRNG) */
-  EMAC_IRQn            = 42, /**< 42 SAM3X8E Ethernet MAC (EMAC) */
-  CAN0_IRQn            = 43, /**< 43 SAM3X8E CAN Controller 0 (CAN0) */
-  CAN1_IRQn            = 44, /**< 44 SAM3X8E CAN Controller 1 (CAN1) */
-
-  PERIPH_COUNT_IRQn    = 45  /**< Number of peripheral IDs */
-} IRQn_Type;
 
 typedef struct _DeviceVectors
 {

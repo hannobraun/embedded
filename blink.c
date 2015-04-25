d1dab3f0cc9116053846b15b54c2358074757f88
Hanno Braun <mail@hannobraun.de>
2015-04-25 11:24:16 +0200

    Make constants static
    They're not read anywhere else, so static is fine.

--- blink.c
+++ blink.c
@@ -1,15 +1,15 @@
 // Addresses of several registers used to control parallel I/O.
-volatile int * const pb_pio_enable          = (int *)0x400E1000;
-volatile int * const pb_output_enable       = (int *)0x400E1010;
-volatile int * const pb_set_output_data     = (int *)0x400E1030;
-volatile int * const pb_clear_output_data   = (int *)0x400E1034;
+static volatile int * const pb_pio_enable          = (int *)0x400E1000;
+static volatile int * const pb_output_enable       = (int *)0x400E1010;
+static volatile int * const pb_set_output_data     = (int *)0x400E1030;
+static volatile int * const pb_clear_output_data   = (int *)0x400E1034;
 
 // Bit mask for PB27. This is pin 13 (the built-in LED) on the Arduino Due.
-const int pb27_mask = 0x08000000;
+static const int pb27_mask = 0x08000000;
 
 // Addresses of several registers used to control the real-time timer.
-volatile int * const timer_mode_register  = (int *)0x400E1A30;
-volatile int * const timer_value_register = (int *)0x400E1A38;
+static volatile int * const timer_mode_register  = (int *)0x400E1A30;
+static volatile int * const timer_value_register = (int *)0x400E1A38;
 
 
 // As the name suggests, this function sleeps for a given number of

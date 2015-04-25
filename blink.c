1c3c4ff07cdf17d277e945215c8b6d191c95565c
Hanno Braun <mail@hannobraun.de>
2015-04-25 11:47:27 +0200

    Document shortcomings of init code
    Yes, all that unnecessary code I removed from Reset_Handler was there
    for a reason. Those TODOs explain in detail what those reasons are (as
    far as I understand them) and why I'm still not doing it right now.

--- blink.c
+++ blink.c
@@ -22,6 +22,39 @@ void sleep_ms(int milliseconds) {
 // This function is the entry point for our application and the handler function
 // for the reset interrupt.
 void start() {
+	// TODO: This function doesn't copy the .relocate segment into RAM, as init
+	//       code would normally do. We're getting away with this, because this
+	//       program doesn't use any global variables (or more generally,
+	//       doesn't have anything that would go into the .data section). Please
+	//       be aware that what might be mistaken for global variables in this
+	//       file are actually global constants, which go into the .rodata
+	//       section. The problem is that if there were global variables, their
+	//       initial value would not be set and the program would just fail
+	//       silently. I see two solutions for this:
+	//       1. Decide that we're never going to use global variables and remove
+	//          support for the .relocate section from the linker script. I
+	//          think if that were done, an attempted write to a global variable
+	//          might fail the program outright, because the global variable
+	//          would reside in ROM. This is just speculation, however, so more
+	//          research is required before implementing this solution.
+	//       2. Just copy the .relocate segment like any sane microcontroller
+	//          program would do. This would definitely be a safe solution, and
+	//          the only reason I'm not doing it right now is that it reeks of
+	//          cargo cult. I'd rather be bitten from not doing it and then have
+	//          a good understanding of why I'm doing it afterwards, than just
+	//          do it from the start without really understanding the reason.
+	// TODO: This function doesn't initialize the .bss segment to zero, as init
+	//       code would normally do. This doesn't make any difference right now,
+	//       because there are no uninitialized global variables in this
+	//       program. I'm wary of just doing it, for two reasons:
+	//       1. I'm not sure why it needs to be done at all. According to my
+	//          understanding, C doesn't guarantee that variables are
+	//          initialized with any given value, so why should global variables
+	//          be different?
+	//       2. Even if there is a good reasons (as there probably is), I don't
+	//          think global variables are such a hot idea, so I don't want to
+	//          do anything that supports them, out of pure stubbornness.
+
 	// Enable PB27 (pin 13) and configure it for output.
 	*pb_pio_enable    = pb27_mask;
 	*pb_output_enable = pb27_mask;

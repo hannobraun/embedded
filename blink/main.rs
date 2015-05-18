f27a15b2510e0a0c0c91cfea333320d57d4d3278
Hanno Braun <mail@hannobraun.de>
2015-05-18 18:35:15 +0200

    Move basic Rust code into a dedicated module

--- blink/main.rs
+++ blink/main.rs
@@ -10,6 +10,8 @@ extern crate core;
 mod pio;
 mod rtt;
 
+pub mod rust_base;
+
 
 use core::prelude::*;
 
@@ -18,27 +20,6 @@ use pio::{
 	PIO_B,
 };
 use rtt::*;
-
-
-// These are a few language items that are required by the core library. The
-// core library is completely platform agnostic and doesn't assume anything
-// (besides very basic things like a stack) about the platform it is running on.
-// Therefore it can't know how to handle program panics and the like.
-
-#[lang = "panic_fmt"]
-pub extern fn panic_fmt() { loop {} }
-
-#[lang = "stack_exhausted"]
-pub extern fn stack_exhausted() { loop {} }
-
-#[lang = "eh_personality"]
-pub extern fn eh_personality() { loop {} }
-
-
-// I'm not 100% sure what this function does, but references to it are compiled
-// into the program by the Rust compiler. I think it would be called in the case
-// of a program panic.
-#[no_mangle] pub extern fn __aeabi_unwind_cpp_pr0() { loop {} }
 
 
 // This is the top of the stack, as provided to us by the linker.

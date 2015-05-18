be97e8e7cf08127a55a9f22708d660d1b0c5d93a
Hanno Braun <mail@hannobraun.de>
2015-05-18 17:29:13 +0200

    Add TODOs

--- blink/main.rs
+++ blink/main.rs
@@ -138,6 +138,14 @@ fn start() {
 // As the name suggests, this function sleeps for a given number of
 // milliseconds. Our replacement for Arduino's delay function.
 fn sleep_ms(milliseconds: u32) {
+	// TODO: Since the timer resolution is 1024 Hz and not 1000 Hz, this
+	//       function is not completely precise. Please don't use it for serious
+	//       timekeeping.
+	// TODO: Even though it might look that way to the untrained eye, this
+	//       function doesn't take overflow into account. Rust's behavior in
+	//       debug mode is to panic, if an integer roperation overflows. While
+	//       this code should work when compiled in release mode, it would be
+	//       much nicer and more reliable to explicitly use wrapping integers.
 	unsafe {
 		let sleep_until = (*RTT).value + milliseconds;
 		while (*RTT).value < sleep_until {}

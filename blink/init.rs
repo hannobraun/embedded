52fc7055ae3cfd6d7d16c6ecb532218f47c065af
Hanno Braun <mail@hannobraun.de>
2015-05-23 09:57:52 +0200

    Extend vector table definition

--- blink/init.rs
+++ blink/init.rs
@@ -27,18 +27,58 @@ extern {
 }
 
 
-// This is a partial definition of the vector table. It only defines the first
-// two entries which, as far as I can tell, are the minimum needed for a program
-// to work at all.
-// Space for the other interrupt handlers is reserved. I'm not sure if this is
-// necessary, but I can imagine that the vector table not having the right
-// length could cause all kinds of problems (imagine if it was too short, and
-// the linker would place something else directly after it).
+// The data structure for the vector table. See data sheet, chapter 10.6.4.
 pub struct VectorTable {
 	pub initial_stack_pointer: &'static u32,
-	pub on_reset             : fn(),
 
-	pub other_interrupt_vectors: [u32; 44],
+	pub on_reset: fn(),
+
+	pub _reserved_1: [u32; 1],
+
+	pub on_hard_fault             : fn(),
+	pub on_memory_management_fault: fn(),
+	pub on_bus_fault              : fn(),
+	pub on_usage_fault            : fn(),
+
+	pub _reserved_2: [u32; 4],
+
+	pub on_sv_call: fn(),
+
+	pub _reserved_3: [u32; 2],
+
+	pub on_pend_sv: fn(),
+	pub on_systick: fn(),
+
+	pub on_irq0 : fn(),
+	pub on_irq1 : fn(),
+	pub on_irq2 : fn(),
+	pub on_irq3 : fn(),
+	pub on_irq4 : fn(),
+	pub on_irq5 : fn(),
+	pub on_irq6 : fn(),
+	pub on_irq7 : fn(),
+	pub on_irq8 : fn(),
+	pub on_irq9 : fn(),
+	pub on_irq10: fn(),
+	pub on_irq11: fn(),
+	pub on_irq12: fn(),
+	pub on_irq13: fn(),
+	pub on_irq14: fn(),
+	pub on_irq15: fn(),
+	pub on_irq16: fn(),
+	pub on_irq17: fn(),
+	pub on_irq18: fn(),
+	pub on_irq19: fn(),
+	pub on_irq20: fn(),
+	pub on_irq21: fn(),
+	pub on_irq22: fn(),
+	pub on_irq23: fn(),
+	pub on_irq24: fn(),
+	pub on_irq25: fn(),
+	pub on_irq26: fn(),
+	pub on_irq27: fn(),
+	pub on_irq28: fn(),
+	pub on_irq29: fn(),
 }
 
 unsafe impl Sync for VectorTable {}
@@ -52,9 +92,56 @@ pub static VECTOR_TABLE: VectorTable = VectorTable {
 	// TODO: Find out why this is &_estack and not just _estack. I was able to
 	//       find documentation stating this fact, but offering no explanation.
 	//       See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0803b/CHDBIJJD.html
-	initial_stack_pointer  : &_estack,
-	on_reset               : on_reset,
-	other_interrupt_vectors: [0; 44],
+	initial_stack_pointer: &_estack,
+
+	on_reset: on_reset,
+
+	_reserved_1: [0; 1],
+
+	on_hard_fault             : abort,
+	on_memory_management_fault: abort,
+	on_bus_fault              : abort,
+	on_usage_fault            : abort,
+
+	_reserved_2: [0; 4],
+
+	on_sv_call: abort,
+
+	_reserved_3: [0; 2],
+
+	on_pend_sv: abort,
+	on_systick: abort,
+
+	on_irq0 : abort,
+	on_irq1 : abort,
+	on_irq2 : abort,
+	on_irq3 : abort,
+	on_irq4 : abort,
+	on_irq5 : abort,
+	on_irq6 : abort,
+	on_irq7 : abort,
+	on_irq8 : abort,
+	on_irq9 : abort,
+	on_irq10: abort,
+	on_irq11: abort,
+	on_irq12: abort,
+	on_irq13: abort,
+	on_irq14: abort,
+	on_irq15: abort,
+	on_irq16: abort,
+	on_irq17: abort,
+	on_irq18: abort,
+	on_irq19: abort,
+	on_irq20: abort,
+	on_irq21: abort,
+	on_irq22: abort,
+	on_irq23: abort,
+	on_irq24: abort,
+	on_irq25: abort,
+	on_irq26: abort,
+	on_irq27: abort,
+	on_irq28: abort,
+	on_irq29: abort,
 };
 
 
@@ -93,4 +180,9 @@ fn on_reset() {
 	//          do anything that supports them, out of pure stubbornness.
 
 	main::start()
+}
+
+// Used as a handler function for all interrupts we don't want to handle yet.
+fn abort() {
+	loop {}
 }

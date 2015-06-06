2db72fce9c610b72e820ed3f223be5f5c43050a2
Hanno Braun <mail@hannobraun.de>
2015-06-06 15:09:40 +0200

    Turn comment into a doc comment

--- blink/hardware/safe/rtt.rs
+++ blink/hardware/safe/rtt.rs
@@ -4,11 +4,11 @@ use hardware::base::rtt::RTT;
 pub struct Timer;
 
 impl Timer {
-	// Create an interface to the timer hardware.
-	// In theory, this should be an unsafe operation, as creating multiple
-	// interfaces to the same hardware will lead to confusion. In practice, the
-	// configuration is hardcoded here, so multiple instances of Timer can
-	// coexist peacefully without confusing each other.
+	/// Create an interface to the timer hardware.
+	/// In theory, this should be an unsafe operation, as creating multiple
+	/// interfaces to the same hardware will lead to confusion. In practice, the
+	/// configuration is hardcoded here, so multiple instances of Timer can
+	/// coexist peacefully without confusing each other.
 	pub fn new() {
 		// Set the timer to a resolution of a millisecond
 		unsafe { (*RTT).mode = 0x00000020; }

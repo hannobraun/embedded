b0ef4b92f15c4d6c2e3dae0801fa8919555dbfbc
Hanno Braun <mail@hannobraun.de>
2016-03-17 17:08:54 +0100

    Convert task into comment

--- blink/src/hardware/safe/rtt.rs
+++ blink/src/hardware/safe/rtt.rs
@@ -21,19 +21,20 @@ impl Timer {
     }
 
     pub fn value(&self) -> u32 {
-        // TODO: This way of reading the timer value may not be accurate.
-        //       According to section 13.4 of the data sheet:
-        //       "As this value can be updated asynchronously from the Master
-        //       Clock, it is advisable to read this register twice at the same
-        //       value to improve accuracy of the returned value."
-        //       I'm not sure what that actually means. Can the value be updated
-        //       in the background, so that only some bits have changed? In that
-        //       case it would make sense to read twice, to make sure the update
-        //       has finished.
-        //       I don't really buy that though. I'm guessing that writing the
-        //       value is atomic and I can't really read some in-between state.
-        //       In that case, reading twice doesn't make any sense and I don't
-        //       really understand what that comment from the data sheet means.
+        // This way of reading the timer value may not be accurate.  According
+        // to section 13.4 of the data sheet:
+        // "As this value can be updated asynchronously from the Master Clock,
+        //  it is advisable to read this register twice at the same value to
+        // improve accuracy of the returned value."
+        //
+        // I'm not sure what that actually means. Can the value be updated in
+        // the background, so that only some bits have changed? In that case
+        // it might make some sense to read twice, to make sure the update has
+        // finished.
+        // I don't really buy that though. I'm guessing that writing the value
+        // is atomic and I can't really read some in-between state. In that
+        // case, reading twice doesn't make any sense and I don't really
+        // understand what that comment from the data sheet means.
         unsafe {
             (*RTT).value.read()
         }

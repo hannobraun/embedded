e53b252de1f45fe2ed84b043a95a9ba1d2237ee4
Hanno Braun <mail@hannobraun.de>
2016-03-17 16:44:13 +0100

    Fix line length

--- blink/src/rust_base.rs
+++ blink/src/rust_base.rs
@@ -3,8 +3,8 @@
 
 // These are a few language items that are required by the core library. The
 // core library is completely platform agnostic and doesn't assume anything
-// (besides very basic things like a stack) about the platform it is running on.
-// Therefore it can't know how to handle program panics and the like.
+// (besides very basic things like a stack) about the platform it is running
+// on. Therefore it can't know how to handle program panics and the like.
 
 #[lang = "panic_fmt"]
 pub extern fn panic_fmt() {
@@ -25,6 +25,6 @@ pub extern fn eh_personality() {
 
 
 // I'm not 100% sure what this function does, but references to it are compiled
-// into the program by the Rust compiler. I think it would be called in the case
-// of a program panic.
+// into the program by the Rust compiler. I think it would be called in the
+// case of a program panic.
 #[no_mangle] pub extern fn __aeabi_unwind_cpp_pr0() { loop {} }

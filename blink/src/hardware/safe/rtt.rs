4d360b3a13e522a2127cde0892344396a669e0be
Hanno Braun <mail@hannobraun.de>
2016-03-17 17:06:55 +0100

    Fix line length

--- blink/src/hardware/safe/rtt.rs
+++ blink/src/hardware/safe/rtt.rs
@@ -8,8 +8,8 @@ pub struct Timer;
 impl Timer {
     /// Create an interface to the timer hardware.
     /// In theory, this should be an unsafe operation, as creating multiple
-    /// interfaces to the same hardware will lead to confusion. In practice, the
-    /// configuration is hardcoded here, so multiple instances of Timer can
+    /// interfaces to the same hardware will lead to confusion. In practice,
+    /// the configuration is hardcoded here, so multiple instances of Timer can
     /// coexist peacefully without confusing each other.
     pub fn new() -> Timer {
         // Set the timer to a resolution of a millisecond

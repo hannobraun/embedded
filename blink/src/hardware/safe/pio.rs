04d8315dc868a63e19e3cf2cf7e5431e100c6979
Hanno Braun <mail@hannobraun.de>
2016-03-17 16:46:15 +0100

    Convert indentation to spaces

--- blink/src/hardware/safe/pio.rs
+++ blink/src/hardware/safe/pio.rs
@@ -15,24 +15,24 @@ pub struct Controller(*mut pio::Controller);
 
 // TODO: Generate functions for all pins with a macro.
 impl Controller {
-	/// Create a new interface to a pin of this controller.
-	///
-	/// This function is unsafe, as only one Pin instance should exist per Pin.
-	/// If multiple instances exist, the compiler can not statically enforce the
-	/// correct use of the API.
-	///
-	/// Initially, both the status and the output status are undefined. This has
-	/// two reasons:
-	/// - The pin might have been used before during the program. We don't know
-	///   what status it was left in.
-	/// - After the system has been powered up, we can't rely on pins being in a
-	///   specific state (see data sheet, chapter 31.5.2).
-	pub unsafe fn pin_27(&self)
-		-> Pin<status::Undefined, output_status::Undefined>
-	{
-		let &Controller(controller) = self;
-		Pin::new(pio::P27, controller)
-	}
+    /// Create a new interface to a pin of this controller.
+    ///
+    /// This function is unsafe, as only one Pin instance should exist per Pin.
+    /// If multiple instances exist, the compiler can not statically enforce the
+    /// correct use of the API.
+    ///
+    /// Initially, both the status and the output status are undefined. This has
+    /// two reasons:
+    /// - The pin might have been used before during the program. We don't know
+    ///   what status it was left in.
+    /// - After the system has been powered up, we can't rely on pins being in a
+    ///   specific state (see data sheet, chapter 31.5.2).
+    pub unsafe fn pin_27(&self)
+        -> Pin<status::Undefined, output_status::Undefined>
+    {
+        let &Controller(controller) = self;
+        Pin::new(pio::P27, controller)
+    }
 }
 
 
@@ -44,25 +44,25 @@ impl Controller {
 // TODO: Pin restricts the capabilities of the underlying hardware severely, by
 //       limiting the operations it supports to only one pin at a time.
 pub struct Pin<Status, OutputStatus> {
-	mask      : u32,
-	controller: *mut pio::Controller,
+    mask      : u32,
+    controller: *mut pio::Controller,
 
-	status       : PhantomData<Status>,
-	output_status: PhantomData<OutputStatus>,
+    status       : PhantomData<Status>,
+    output_status: PhantomData<OutputStatus>,
 }
 
 impl<Status, OutputStatus> Pin<Status, OutputStatus> {
-	fn new(mask: u32, controller: *mut pio::Controller)
-		-> Pin<Status, OutputStatus>
-	{
-		Pin {
-			mask      : mask,
-			controller: controller,
+    fn new(mask: u32, controller: *mut pio::Controller)
+        -> Pin<Status, OutputStatus>
+    {
+        Pin {
+            mask      : mask,
+            controller: controller,
 
-			status       : PhantomData,
-			output_status: PhantomData,
-		}
-	}
+            status       : PhantomData,
+            output_status: PhantomData,
+        }
+    }
 }
 
 // TODO: This type parameters for this implementation block may not be correct.
@@ -70,46 +70,46 @@ impl<Status, OutputStatus> Pin<Status, OutputStatus> {
 //       or enabled. This may not be the case. It might be safer to always
 //       return a pin with output_status::Undefined.
 impl<OutputStatus> Pin<status::Undefined, OutputStatus> {
-	pub fn enable(self) -> Pin<status::Enabled, OutputStatus> {
-		unsafe {
-			(*self.controller).pio_enable.write(self.mask)
-		}
+    pub fn enable(self) -> Pin<status::Enabled, OutputStatus> {
+        unsafe {
+            (*self.controller).pio_enable.write(self.mask)
+        }
 
-		Pin::new(self.mask, self.controller)
-	}
+        Pin::new(self.mask, self.controller)
+    }
 }
 
 impl Pin<status::Enabled, output_status::Undefined> {
-	pub fn enable_output(self) -> Pin<status::Enabled, output_status::Enabled> {
-		unsafe {
-			(*self.controller).output_enable.write(self.mask)
-		}
+    pub fn enable_output(self) -> Pin<status::Enabled, output_status::Enabled> {
+        unsafe {
+            (*self.controller).output_enable.write(self.mask)
+        }
 
-		Pin::new(self.mask, self.controller)
-	}
+        Pin::new(self.mask, self.controller)
+    }
 }
 
 impl Pin<status::Enabled, output_status::Enabled> {
-	pub fn set_output(&self) {
-		unsafe {
-			(*self.controller).set_output_data.write(self.mask)
-		}
-	}
+    pub fn set_output(&self) {
+        unsafe {
+            (*self.controller).set_output_data.write(self.mask)
+        }
+    }
 
-	pub fn clear_output(&self) {
-		unsafe {
-			(*self.controller).clear_output_data.write(self.mask)
-		}
-	}
+    pub fn clear_output(&self) {
+        unsafe {
+            (*self.controller).clear_output_data.write(self.mask)
+        }
+    }
 }
 
 
 mod status {
-	pub struct Undefined;
-	pub struct Enabled;
+    pub struct Undefined;
+    pub struct Enabled;
 }
 
 mod output_status {
-	pub struct Undefined;
-	pub struct Enabled;
+    pub struct Undefined;
+    pub struct Enabled;
 }

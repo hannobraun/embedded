cdf0a83d91bc4432a97180bf614f7279f570f23f
Hanno Braun <mail@hannobraun.de>
2016-04-17 09:03:44 +0200

    Enable RTT interrupts

--- blink/src/program.rs
+++ blink/src/program.rs
@@ -1,9 +1,13 @@
+use hardware::safe::nvic::Nvic;
 use hardware::safe::pio;
 use hardware::safe::rtt::Timer;
 use hardware::safe::wdt::restart_watchdog;
 
 
 pub fn start() {
+    let mut nvic = unsafe { Nvic::new() };
+    nvic.enable_rtt();
+
     // Pin 27 of the PIOB parallel I/O controller corresponds to pin 13 on the
     // Arduino Due, which is the built-in LED (labelled "L").
     let led = unsafe { pio::b().pin_27() };

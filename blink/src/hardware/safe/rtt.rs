01dfbc00bd5b55cb0d3e9decf2c3e8a94e473b44
Hanno Braun <mail@hannobraun.de>
2016-04-25 10:26:44 +0200

    Convert TODO into comment

--- blink/src/hardware/safe/rtt.rs
+++ blink/src/hardware/safe/rtt.rs
@@ -60,7 +60,12 @@ impl Timer {
         }
     }
 
-    /// Sleep for the given number of milliseconds.
+    /// Sleep for a period of time roughly corresponding to the given number
+    /// of milliseconds.
+    ///
+    /// The timer is set to a frequency of 1024 Hz, not 1000 Hz, which means
+    /// the waiting time is off by about 2.4%. Please don't use this method for
+    /// serious timekeeping.
     ///
     /// This function configures the RTT and the NVIC as required, but doesn't
     /// currently reset the configuration. This doesn't matter in the context
@@ -87,9 +92,6 @@ impl Timer {
 
         nvic.enable_rtt();
 
-        // TODO: Since the timer resolution is 1024 Hz and not 1000 Hz, this
-        //       function is not completely precise. Please don't use it for
-        //       serious timekeeping.
         // TODO: This function doesn't really sleep. Rather, it waits busily,
         //       wasting a lot of resources.
         // TODO: The behavior of this function is wrong, if the addition wraps.

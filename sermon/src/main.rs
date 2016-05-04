53ded5f32af71b606f69665b00a547cb29453c59
Hanno Braun <mail@hannobraun.de>
2016-05-04 10:08:11 +0200

    Extend sermon with write capability
    It will now write lines from stdin to the serial connection.

--- sermon/src/main.rs
+++ sermon/src/main.rs
@@ -2,6 +2,13 @@ extern crate serial;
 
 
 use std::io;
+use std::io::prelude::*;
+use std::sync::mpsc::{
+    channel,
+    Receiver,
+    TryRecvError,
+};
+use std::thread;
 
 use serial::prelude::*;
 
@@ -12,12 +19,23 @@ fn main() {
     let mut serial_port = open_port(path)
         .expect("Failed to open serial port");
 
+    let input = start_input_reader();
+
     loop {
         if let Err(error) = io::copy(&mut serial_port, &mut io::stdout()) {
             if error.kind() != io::ErrorKind::TimedOut {
                 panic!("Failed to print serial output: {}");
             }
         }
+
+        match input.try_recv() {
+            Ok(input) =>
+                serial_port.write_all(input.as_bytes())
+                    .expect("Failed to write to serial port"),
+
+            Err(TryRecvError::Empty)        => (),
+            Err(TryRecvError::Disconnected) => panic!("Channel disconnected"),
+        };
     }
 }
 
@@ -35,4 +53,22 @@ fn open_port(path: &str) -> serial::Result<serial::SystemPort> {
     }));
 
     Ok(port)
+}
+
+fn start_input_reader() -> Receiver<String> {
+    let (sender, receiver) = channel();
+
+    thread::spawn(move || {
+        let stdin = io::stdin();
+
+        loop {
+            let mut input = String::new();
+            stdin.read_line(&mut input)
+                .expect("Failed to read from stdin");
+            sender.send(input)
+                .expect("Failed to send input via channel");
+        }
+    });
+
+    receiver
 }

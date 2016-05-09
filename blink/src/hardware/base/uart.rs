229add10980efd2c6dcc21de06410d66e8e2e1d6
Hanno Braun <mail@hannobraun.de>
2016-05-09 09:48:16 +0200

    Improve formatting

--- blink/src/hardware/base/uart.rs
+++ blink/src/hardware/base/uart.rs
@@ -10,14 +10,14 @@ use super::pdc::Pdc;
 /// UART user interface. See data sheet, chapter 34.6.
 #[repr(C)]
 pub struct Uart {
-    pub control: Volatile<u32>,
-    pub mode   : Volatile<u32>,
-    pub interrupt_enable: Volatile<u32>,
-    pub interrupt_disable: Volatile<u32>,
-    pub interrupt_mask   : Volatile<u32>,
-    pub status           : Volatile<u32>,
-    pub receive_holding  : Volatile<u32>,
-    pub transmit_holding : Volatile<u32>,
+    pub control            : Volatile<u32>,
+    pub mode               : Volatile<u32>,
+    pub interrupt_enable   : Volatile<u32>,
+    pub interrupt_disable  : Volatile<u32>,
+    pub interrupt_mask     : Volatile<u32>,
+    pub status             : Volatile<u32>,
+    pub receive_holding    : Volatile<u32>,
+    pub transmit_holding   : Volatile<u32>,
     pub baud_rate_generator: Volatile<u32>,
 
     _reserved: [Volatile<u32>; 55],

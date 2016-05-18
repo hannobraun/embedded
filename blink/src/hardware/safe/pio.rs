99037148439ae66edadb003bc66646f6a5d3e19b
Hanno Braun <mail@hannobraun.de>
2016-05-18 11:06:16 +0200

    Define missing pin methods

--- blink/src/hardware/safe/pio.rs
+++ blink/src/hardware/safe/pio.rs
@@ -44,7 +44,38 @@ macro_rules! impl_controller {
 // Generate `Controller` implementation. Each line generates a function that
 // returns a specific pin. More functions can be added as needed.
 impl_controller! {
+    0 , pin_0 , P0 ;
+    1 , pin_1 , P1 ;
+    2 , pin_2 , P2 ;
+    3 , pin_3 , P3 ;
+    4 , pin_4 , P4 ;
+    5 , pin_5 , P5 ;
+    6 , pin_6 , P6 ;
+    7 , pin_7 , P7 ;
+    8 , pin_8 , P8 ;
+    9 , pin_9 , P9 ;
+    10, pin_10, P10;
+    11, pin_11, P11;
+    12, pin_12, P12;
+    13, pin_13, P13;
+    14, pin_14, P14;
+    15, pin_15, P15;
+    16, pin_16, P16;
+    17, pin_17, P17;
+    18, pin_18, P18;
+    19, pin_19, P19;
+    20, pin_20, P20;
+    21, pin_21, P21;
+    22, pin_22, P22;
+    23, pin_23, P23;
+    24, pin_24, P24;
+    25, pin_25, P25;
+    26, pin_26, P26;
     27, pin_27, P27;
+    28, pin_28, P28;
+    29, pin_29, P29;
+    30, pin_30, P30;
+    31, pin_31, P31;
 }
 
 

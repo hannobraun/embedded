f80eef903bc5ecc90f99351be8ab08cbd152d077
Hanno Braun <mail@hannobraun.de>
2016-05-19 10:28:32 +0200

    Add logging to main loop

--- blink/src/program.rs
+++ blink/src/program.rs
@@ -1,6 +1,9 @@
+use core::fmt::Write;
+
 use hardware::safe::nvic::Nvic;
 use hardware::safe::pio;
 use hardware::safe::rtt::sleep_ms;
+use hardware::safe::uart::Uart;
 use hardware::safe::wdt::restart_watchdog;
 
 
@@ -14,7 +17,15 @@ pub fn start() {
         .enable()
         .enable_output();
 
+    let     uart_tx = unsafe { pio::a().pin_9() };
+    let mut uart    = unsafe { Uart::new(uart_tx) };
+
     loop {
+        // Ignore logging errors. If there are any, it's not worth killing the
+        // program over the debug output. It would be nicer to safe them and
+        // report them later, however.
+        let _ = write!(uart, "Start main loop iteration\n");
+
         restart_watchdog();
 
         led.set_output();

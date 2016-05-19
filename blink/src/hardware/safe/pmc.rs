4c3e984d24947918c975c09fdd6f5a712f1c55aa
Hanno Braun <mail@hannobraun.de>
2016-05-19 09:31:16 +0200

    Add function that returns main clock frequency

--- blink/src/hardware/safe/pmc.rs
+++ blink/src/hardware/safe/pmc.rs
@@ -1,4 +1,9 @@
-use hardware::base::pmc::PMC;
+use hardware::base::pmc::{
+    MAINF_MASK,
+    MAINFRDY,
+    PMC,
+    SLOW_CLOCK_FREQUENCY_HZ,
+};
 use hardware::safe::peripherals::Peripheral;
 
 
@@ -6,4 +11,14 @@ pub fn enable_peripheral_clock_0(peripheral: &Peripheral) {
     unsafe {
         (*PMC).peripheral_clock_enable_0.write(peripheral.mask());
     }
+}
+
+pub fn main_clock_frequency_hz() -> u32 {
+    let main_clock_frequency_within_16_slow_clock_cycles = unsafe {
+        while (*PMC).main_clock_frequency.read() & MAINFRDY == 0 {}
+        (*PMC).main_clock_frequency.read() & MAINF_MASK
+    };
+
+    main_clock_frequency_within_16_slow_clock_cycles
+        * SLOW_CLOCK_FREQUENCY_HZ / 16
 }

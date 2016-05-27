fe685ca2670ddc48f8a43558e5bef349987413b6
Hanno Braun <mail@hannobraun.de>
2016-05-27 09:01:50 +0200

    Use correct signature for `panic_fmt` lang item

--- blink/src/rust_base.rs
+++ blink/src/rust_base.rs
@@ -1,13 +1,20 @@
 // Some basic stuff that is required to get Rust and its core library to work.
 
 
+use core::fmt::Arguments;
+
+
 // These are a few language items that are required by the core library. The
 // core library is completely platform agnostic and doesn't assume anything
 // (besides very basic things like a stack) about the platform it is running
 // on. Therefore it can't know how to handle program panics and the like.
 
 #[lang = "panic_fmt"]
-pub extern fn panic_fmt() {
+pub extern fn rust_begin_unwind(
+	_message: &Arguments,
+	_file   : &'static str,
+	_line   : u32,
+) -> ! {
     // TODO: Print a message to serial output, if available and reset the
     //       system.
     loop {}

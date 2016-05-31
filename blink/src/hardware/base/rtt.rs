a13175c3589648d17709e2f37aa84904754a3503
Hanno Braun <mail@hannobraun.de>
2016-05-31 08:40:47 +0200

    Add constants for RTT mode bits

--- blink/src/hardware/base/rtt.rs
+++ blink/src/hardware/base/rtt.rs
@@ -15,4 +15,10 @@ pub struct Rtt {
 }
 
 
+// Mode register flags. See data sheet, section 13.5.1.
+pub const ALMIEN   : u32 = 0x1 << 16; // Alarm Interrupt Enable
+pub const RTTINCIEN: u32 = 0x1 << 17; // RTT Increment Interrupt Enable
+pub const RTTRST   : u32 = 0x1 << 18; // RTT Restart
+
+
 pub const RTT: *mut Rtt = 0x400E1A30 as *mut Rtt;

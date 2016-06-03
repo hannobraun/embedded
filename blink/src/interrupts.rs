e27a7f6b9f25a32966457b17bfdac98ca4d452a9
Hanno Braun <mail@hannobraun.de>
2016-06-03 09:12:49 +0200

    Add documentation

--- blink/src/interrupts.rs
+++ blink/src/interrupts.rs
@@ -1,9 +1,20 @@
+/// Functions for enabling and disabling interrupts.
+/// The function documentation refers to the ARM Cortex-M Programming Guide to
+/// Memory Barrier Instructions (Application Note 321), available online at:
+/// http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dai0321a/index.html
+
+
+/// Enable interrupts.
 pub fn enable() {
     unsafe {
         asm!("cpsie i" :::: "volatile")
     };
 }
 
+/// Disable interrupts.
+/// According to Application Note 321, section 3.3, this instruction is self
+/// synchronizing, which means the interrupts are disabled after this
+/// instruction completes, and no explicit memory barrier is required.
 pub fn disable() {
     unsafe {
         asm!("cpsid i" :::: "volatile");

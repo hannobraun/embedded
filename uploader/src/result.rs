62cab959ad3c87d1a0fb086680c15be2b7fb5c87
Hanno Braun <mail@hannobraun.de>
2016-08-22 13:52:36 +0200

    uploader: Update dependencies

--- uploader/src/result.rs
+++ uploader/src/result.rs
@@ -1,22 +1,13 @@
 use std::io;
 use std::result;
 
-use byteorder;
-
 
 pub type Result<T> = result::Result<T, Error>;
 
 
 #[derive(Debug)]
 pub enum Error {
-    ByteOrder(byteorder::Error),
     Io(io::Error),
-}
-
-impl From<byteorder::Error> for Error {
-    fn from(error: byteorder::Error) -> Self {
-        Error::ByteOrder(error)
-    }
 }
 
 impl From<io::Error> for Error {
